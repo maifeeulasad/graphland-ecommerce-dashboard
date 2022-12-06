fb6e19e3afb3ecda5dde6ac51428e9424355fd19
Maifee Ul Asad <maifeeulasad@gmail.com>
2022-12-06 19:53:24 +0600

    product list

--- src/App.tsx
+++ src/App.tsx
@@ -9,9 +9,9 @@ import {
 
 import { CustomLayout } from './layout/CustomLayout';
 import { Landing } from './component/landing/Landing';
-import { Page2 } from './component/Page2';
 import { Page3 } from './component/Page3';
 import { Login } from './component/auth/login/Login';
+import { ProductList } from './component/product/ProductList';
 
 const DEFAULT_ROUTE = '/login';
 
@@ -32,7 +32,7 @@ const App = () => (
         <Route path="/landing" element={<Landing />} />
         <Route path="/login" element={<Login />} />
         <Route element={<RequireAuth />}>
-          <Route path="/page2" element={<Page2 />} />
+          <Route path="/product-list" element={<ProductList />} />
           <Route path="/page3" element={<Page3 />} />
         </Route>
         <Route
---
+++ src/component/product/ProductList.tsx
@@ -0,0 +1,78 @@
+import React, { useEffect, useState } from 'react';
+import Button from 'antd/lib/button';
+import Table from 'antd/lib/table';
+import Space from 'antd/lib/space';
+import { network } from '../../network/network';
+
+import styles from './ProductList.module.scss';
+
+const columns = [
+  {
+    title: 'ID',
+    dataIndex: 'id',
+    key: 'id',
+    width: '20%',
+  },
+  {
+    title: 'Title',
+    dataIndex: 'title',
+    key: 'title',
+    width: '20%',
+  },
+  {
+    title: 'Price',
+    dataIndex: 'price',
+    key: 'price',
+    width: '20%',
+  },
+  {
+    title: 'Image',
+    dataIndex: 'cover_image',
+    key: 'image',
+    width: '20%',
+    render: (_: any, record: any) => (
+      // eslint-disable-next-line jsx-a11y/img-redundant-alt
+      <img
+        src={
+          record.cover_photo ?
+            `http://104.251.211.125:8055/assets/${record.cover_photo}?width=80` :
+            'http://104.251.211.125:8055/assets/58d3581a-b889-408c-9d5e-102fb79b570f?width=80'
+        }
+        alt="Image cover"
+      />
+    ),
+  },
+  {
+    title: 'Actions',
+    key: 'actions',
+    render: () => (
+      <Space align="center">
+        <Button type="danger">Delete</Button>
+        <Button type="primary">Edit</Button>
+      </Space>
+    ),
+    width: '20%',
+  },
+];
+
+const ProductList = () => {
+  const [products, setProducts] = useState([]);
+
+  const fetchProductList = () => {
+    network.get('/items/products').then((res) => setProducts(res.data.data));
+  };
+
+  useEffect(() => {
+    fetchProductList();
+  }, []);
+
+  return (
+    <Table
+      rowClassName={(_, index) => (index % 2 ? styles.grayish : '')}
+      columns={columns}
+      dataSource={products}
+    />
+  );
+};
+
+export { ProductList };
