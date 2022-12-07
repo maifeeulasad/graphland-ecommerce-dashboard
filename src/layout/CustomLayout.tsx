bdde1580af924a19b7550a890ddea81cad413242
Maifee Ul Asad <maifeeulasad@gmail.com>
2022-12-07 07:36:43 +0600

    [ui]: enhance layout + logic

--- src/layout/CustomLayout.tsx
+++ src/layout/CustomLayout.tsx
@@ -1,7 +1,7 @@
-import React from 'react';
+import React, { useMemo } from 'react';
 import Layout from 'antd/lib/layout';
 import PageHeader from 'antd/lib/page-header';
-import { Link, useNavigate } from 'react-router-dom';
+import { Link, useNavigate, useLocation } from 'react-router-dom';
 
 import 'overlayscrollbars/overlayscrollbars.css';
 import { OverlayScrollbarsComponent } from 'overlayscrollbars-react';
@@ -10,17 +10,18 @@ const { Header, Content, Footer } = Layout;
 
 const CustomHeader = () => {
   const navigate = useNavigate();
+  const location = useLocation();
+
+  const isLoginPage = useMemo(() => location.pathname.includes('/login'), [location]);
+
   return (
     <PageHeader
       className="site-page-header h-full"
-      title={<Link to="/">React Pipeline</Link>}
-      subTitle="SPA using React + TS + Vite + Tailwind"
-      onBack={() => navigate(-1)}
+      title={<Link to={isLoginPage ? '/' : '/product-list'}>ECom</Link>}
+      subTitle="Graphland Ecommerce Dashboard"
+      onBack={!isLoginPage ? () => navigate(-1) : undefined}
       extra={
-        <>
-          <Link to="/product-list">Product List</Link>
-          <Link to="/page3">Page 3</Link>
-        </>
+        isLoginPage ? <div /> : <Link to="/product-list">Product List</Link>
       }
     />
   );
